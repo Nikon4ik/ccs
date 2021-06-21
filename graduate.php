958aafd41b574e0087505544144abe8e619b57e2
Oleh Zaichenko <frenkbober66@gmail.com>
2021-06-22 01:44:32 +0300

    first commit

---
+++ graduate.php
@@ -0,0 +1,34 @@
+{
+  "company": [
+    {
+      "country": "Ukraine",
+      "city": "Odessa",
+      "company": "Lohika",
+      "geocode": "46.4802166623439, 30.74792735406774"
+    },
+    {
+      "country": "Ukraine",
+      "city": "Kyiv",
+      "company": "Data Art",
+      "geocode": "50.45488312218608, 30.476464999374294"
+    },
+    {
+      "country": "Poland",
+      "city": "Krakow",
+      "company": "Luxoft",
+      "geocode": "50.10616623204983, 19.824205706587822"
+    },
+    {
+      "country": "USA",
+      "city": "Chicago",
+      "company": "Planmeca",
+      "geocode": "42.069588542104256, -88.20006159691205"
+    },
+    {
+      "country": "Germany",
+      "city": "Munich",
+      "company": "TUM",
+      "geocode": "48.148929843565256, 11.567838739738313"
+    }
+  ]
+}
